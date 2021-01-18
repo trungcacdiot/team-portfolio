603bb9add7e0011ef95bf18f47b15d6156d78226
trung <buibatrung01@gmail.com>
2021-01-18 20:38:52 +0700

    contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'
+import Contact from './pages/Contact/Contact'
 import Projects from './pages/Projects'
 
 function App() {
